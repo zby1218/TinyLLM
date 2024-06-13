6424c5fa7ed06eb11e327ac55da5b8246f8eeb75
ZhangBoYu <617025999@qq.com>
2024-06-13 15:23:56 +0900

    encode unknow word

--- vocabulary.py
+++ vocabulary.py
@@ -10,6 +10,9 @@ words = re.split(r'([,.?_!"()\']|--|\s)',raw_text)
 # 标点被识别为空格，去除空格
 words =  [word.strip() for word in words if word.strip()]
 # 将字符转换为set类型，并进行排序
-all_words = sorted(set(words))
-print(all_words[:50])
+all_words = sorted(list(set(words)))
+# 添加两个字符，一个是unknown 一个是endoftext
+# unk是不在vocabulary中的字符，endoftext是不同文本之间的分隔符
+all_words.extend(["<|endoftext|>","<|unk|>"])
+print(len(all_words))
 vocabulary = {token: index for index, token in enumerate(all_words)}
